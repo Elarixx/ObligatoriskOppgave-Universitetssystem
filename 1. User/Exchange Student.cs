fd97f1992cd3199d1264e2afc4e652212e8c86c4
Elarixx <theamste@uia.no>
2026-03-08 23:09:03 +0100

    Add project files.

---
+++ 1. User/Exchange Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obligatorisk_Oppgave_1_Universitetssystem._1._User
+{
+    internal class Exchange_Student
+    {
+    }
+}
